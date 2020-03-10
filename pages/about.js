231b327974deff21418033ccbd1f70cf71218baf
Amel <amcenp@gmail.com>
2020-03-10 19:18:50 +0100

    Data fetched from backend

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,25 @@
 import Layout from '../components/Layout';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
+export default class About extends Component {
+  static async getInitialProps() {
+    const response = await fetch(`https://api.github.com/users/ammce`);
+    const data = await response.json();
+    return {
+      user: data,
+    };
+  }
 
-const About = () => {
-  return (
-    <Layout title='About Page'>
-      <p>I am a full stack developer</p>
-      <img src='/static/jslogo.png' alt='fullstack-js' height='200px' />
-    </Layout>
-  );
-};
-
-export default About;
+  render() {
+    const { user } = this.props;
+    console.log(this.props);
+    return (
+      <Layout title='About Page'>
+        <p>{user.name}</p>
+        <img src={user.avatar_url} height='200px' />
+        <p>{user.bio}</p>
+        <img src='/static/jslogo.png' alt='fullstack-js' height='200px' />
+      </Layout>
+    );
+  }
+}
